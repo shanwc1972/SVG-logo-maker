6fc84a9a57d8c894d40beccb312001a81f2553d6
shanwc1972 <166612646+shanwc1972@users.noreply.github.com>
2024-08-09 22:09:12 +1000

    starter code

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const gsvg = require('./lib/gensvg.js');
+
+// Below is a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created logo.svg file')
+      );
+}
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'threechars',
+      message: 'Please enter three characters',
+    },
+    {
+      type: 'input',
+      name: 'textcolor',
+      message: 'Please enter a text color',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Please enter a shape',
+      choices: ['circle', 'square', 'triangle'],
+    },
+    {
+      type: 'input',
+      name: 'shapecolor',
+      message: 'Please enter the shapes color',
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+    const xmlSVG = gsvg.gensvg(answers);
+    console.log(xmlSVG);
+    //writeToFile('./examples/logo.svg', xmlSVG);
+  });
